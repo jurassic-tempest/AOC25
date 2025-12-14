2b78c5a7f9b9403345373142e107510c2af31278
Pamela Mezue <pamelamezue@Pamelas-MacBook-Pro.local>
2025-12-14 11:56:24 +0000

    day 6 part one

--- AOC25/Solutions/Day06.cs
+++ AOC25/Solutions/Day06.cs
@@ -5,18 +5,62 @@ namespace AOC.Solutions {
         private readonly string[] input;
 
         public Day06() {
-            input = Utils.ReadInput(5); // loads Inputs/day01.txt
+            input = Utils.ReadInput(6); 
         }
 
         public string Part1() {
-            // TODO: write your solution here
-            int result = 0;
+            long result = 0;
+            var lineCount = Utils.GetLinesCount(6);
+            List<List<int>> numbers = BuildNumbersArrays(lineCount, input);
+            List<string> operators = GetOperators();
+            var largestLength = numbers.Select(x => x.Count).Max();
 
-            foreach (var line in input) {
-                // your logic
+            for (int i = 0; i < largestLength; i++)
+            {
+                var op = operators[i];
+                var nums = numbers.Select(x => x[i]);
+                if (op == "*")
+                {
+                    long current = 1;
+                    foreach(var num in nums)
+                    {
+                        current = current * num;
+                    }
+                    result += current;
+                }
+                else
+                {
+                    long current = 0;
+                    foreach(var num in nums)
+                    {
+                        current = current + num;
+                    }
+                    result += current;
+                }
             }
 
             return result.ToString();
+        }
+
+        private List<List<int>> BuildNumbersArrays(int lineCount, string[] input)
+        {
+            List<List<int>> result = new List<List<int>>();
+
+            for(int i = 0; i < lineCount-1; i++)
+            {
+                // split multiple spaces
+                var arr = input[i].Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+                result.Add(arr.Select(int.Parse).ToList());
+            }
+            return result;
+        }
+
+        private List<string> GetOperators()
+        {
+            var operators = Utils.ReadLastLine(6);
+            var ops = operators.Split((char[])null, StringSplitOptions.RemoveEmptyEntries).ToList();
+            Console.WriteLine($"ops: {ops}");
+            return ops;
         }
 
         public string Part2() {
--- AOC25/Utils.cs
+++ AOC25/Utils.cs
@@ -8,6 +8,18 @@ public static class Utils
         return File.ReadAllLines(path);
     }
 
+    public static string? ReadLastLine(int day)
+    {
+        var path = $"Inputs/day{day:00}.txt";
+        return File.ReadLines(path).LastOrDefault();
+    }
+
+    public static int GetLinesCount(int day)
+    {
+        var path = $"Inputs/day{day:00}.txt";
+        return File.ReadLines(path).Count();
+    }
+
     public static string ReadInputRaw(int day)
     {
         var path = $"Inputs/day{day:00}.txt";
